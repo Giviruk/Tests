36779a6cb672078e010834af64b5ce247c7eda85
Egor <malyshkin@hightech.group>
2020-11-09 15:16:08 +0300

    addProperties

--- TestProject1/TestProject1/ApplicationManager.cs
+++ TestProject1/TestProject1/ApplicationManager.cs
@@ -9,46 +9,46 @@ namespace TestProject1
     public class ApplicationManager
     {
         public readonly IWebDriver Driver;
-        public IDictionary<string, object> Vars { get; set; }
-        public IJavaScriptExecutor _js;
-        private LoginHelper _loginHelper;
-        private NavigationHelper _navigationHelper;
-        private CreateDirectoryHelper _createDirectoryHelper;
-        public AuthData AuthData;
+        private IDictionary<string, object> Vars { get; set; }
+        private IJavaScriptExecutor Js { get; set; }
+        private LoginHelper LoginHelper { get; set; }
+        private NavigationHelper NavigationHelper { get; set; }
+        private CreateDirectoryHelper CreateDirectoryHelper { get; set; }
+        private AuthData AuthData { get; set; }
 
         public ApplicationManager()
         {
             Driver = new ChromeDriver("C:\\Users\\malyshkin\\Desktop");
-            _js = (IJavaScriptExecutor) Driver;
+            Js = (IJavaScriptExecutor) Driver;
             Vars = new Dictionary<string, object>();
-            _loginHelper = new LoginHelper(this);
-            _navigationHelper = new NavigationHelper(this);
-            _createDirectoryHelper = new CreateDirectoryHelper(this);
+            LoginHelper = new LoginHelper(this);
+            NavigationHelper = new NavigationHelper(this);
+            CreateDirectoryHelper = new CreateDirectoryHelper(this);
             AuthData = new AuthData("givi111222333", "1q2w3eqazwsxedc");
         }
 
         public void Auth(AuthData authData)
         {
             GoToLogin();
-            _loginHelper.Login(authData);
+            LoginHelper.Login(authData);
         }
 
         public void CreateDirectory(string folderName)
         {
             Auth(AuthData);
             GoToMailBox();
-            _createDirectoryHelper.CreateDirectory(folderName);
+            CreateDirectoryHelper.CreateDirectory(folderName);
         }
 
         public void GoToLogin()
         {
-            _navigationHelper.GoTo(
+            NavigationHelper.GoTo(
                 "https://passport.yandex.ru/auth?origin=home_desktop_ru&retpath=https%3A%2F%2Fmail.yandex.ru%2F&backpath=https%3A%2F%2Fyandex.ru");
         }
 
         public void GoToMailBox()
         {
-            _navigationHelper.GoTo("https://mail.yandex.ru/?uid=1215779683#inbox");
+            NavigationHelper.GoTo("https://mail.yandex.ru/?uid=1215779683#inbox");
         }
     }
 }
